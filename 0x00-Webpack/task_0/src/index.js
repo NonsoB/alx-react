1b3c6df947aa6045b6b8898c6466b036df99d841
“NonsoB” <okorojichinonso1@gmail.com>
2024-06-26 05:55:17 -0700

    code generated successfully

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,5 +1,7 @@
 import $ from 'jquery';
 
 $(document).ready(function() {
-  $('body').html('<h1>Hello, Webpack!</h1>');
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
 });
